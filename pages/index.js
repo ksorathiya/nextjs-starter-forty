dd22bb1f121ca21dcd001d03238a8530f9ccca64
kartik <sorathiyakartik@gmail.com>
2021-08-07 08:58:54 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,46 +12,46 @@ export default () => (
                 <section id="one" className="tiles">
                     <article style={{backgroundImage: `url('/static/images/pic01.jpg')`}}>
                         <header className="major">
-                            <h3>Aliquam</h3>
-                            <p>Ipsum dolor sit amet</p>
+                            <h3>My Life</h3>
+                            <p>This is my life!</p>
                         </header>
                         <Link href="/landing"><a className="link primary"></a></Link>
                     </article>
                     <article style={{backgroundImage: `url('/static/images/pic02.jpg')`}}>
                         <header className="major">
-                            <h3>Tempus</h3>
+                            <h3>My Work</h3>
                             <p>feugiat amet tempus</p>
                         </header>
                         <Link href="/landing"><a className="link primary"></a></Link>
                     </article>
                     <article style={{backgroundImage: `url('/static/images/pic03.jpg')`}}>
                         <header className="major">
-                            <h3>Magna</h3>
+                            <h3>My Car</h3>
                             <p>Lorem etiam nullam</p>
                         </header>
                         <Link href="/landing"><a className="link primary"></a></Link>
                     </article>
                     <article style={{backgroundImage: `url('/static/images/pic04.jpg')`}}>
                         <header className="major">
-                            <h3>Ipsum</h3>
+                            <h3>My Friends</h3>
                             <p>Nisl sed aliquam</p>
                         </header>
                         <Link href="/landing"><a className="link primary"></a></Link>
                     </article>
-                    <article style={{backgroundImage: `url('/static/images/pic05.jpg')`}}>
-                        <header className="major">
-                            <h3>Consequat</h3>
-                            <p>Ipsum dolor sit amet</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic06.jpg')`}}>
-                        <header className="major">
-                            <h3>Etiam</h3>
-                            <p>Feugiat amet tempus</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
+//                     <article style={{backgroundImage: `url('/static/images/pic05.jpg')`}}>
+//                         <header className="major">
+//                             <h3>Consequat</h3>
+//                             <p>Ipsum dolor sit amet</p>
+//                         </header>
+//                         <Link href="/landing"><a className="link primary"></a></Link>
+//                     </article>
+//                     <article style={{backgroundImage: `url('/static/images/pic06.jpg')`}}>
+//                         <header className="major">
+//                             <h3>Etiam</h3>
+//                             <p>Feugiat amet tempus</p>
+//                         </header>
+//                         <Link href="/landing"><a className="link primary"></a></Link>
+//                     </article>
                 </section>
                 <section id="two">
                     <div className="inner">
